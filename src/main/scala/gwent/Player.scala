227e3bd566262305e05cf6577a3aa3c942df7668
itzluzy <luxcc2002@gmail.com>
2023-04-09 00:19:48 -0400

    TEST: Added test structure for the Player class

---
+++ src/main/scala/gwent/Player.scala
@@ -0,0 +1,5 @@
+package cl.uchile.dcc
+package gwent
+
+class Player {
+}
---
+++ src/test/scala/gwent/PlayerTest.scala
@@ -0,0 +1,39 @@
+package cl.uchile.dcc
+package gwent
+import munit.FunSuite
+
+class PlayerTest extends FunSuite{
+  var name: String = null
+  var section: Section = null
+  var gems: Int = null
+  var deck: Deck = null
+  var hand : Hand = null
+  var player: Player = null
+
+  override def beforeEach(context: BeforeEach): Unit ={
+    name = "Gyro"
+    section = new Section()
+    gems = 2
+    deck = new Deck()
+    hand = new Hand()
+    player = new Player("Gyro")
+  }
+
+  test("Player has well defined attributes"){
+    assertEquals(player.name, name)
+    assertEquals(player.section, section)
+    assertEquals(player.gems, gems)
+    assertEquals(player.getDeck(), deck)
+    assertEquals(player.getHand(), hand)
+  }
+
+  test("When the Player plays a card, it is no longer in their hand"){
+
+  }
+
+  test("When the Player draws a card, it goes from their deck to their hand"){
+
+  }
+
+}
+
