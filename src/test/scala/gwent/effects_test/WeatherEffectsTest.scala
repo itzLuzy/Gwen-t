9a750789eea45a63c9e722d0dff935cb3601e67b
itzluzy <luxcc2002@gmail.com>
2023-07-17 22:28:05 -0400

    Added more tests for effects

--- src/test/scala/gwent/effects_test/WeatherEffectsTest.scala
+++ src/test/scala/gwent/effects_test/WeatherEffectsTest.scala
@@ -11,7 +11,7 @@ import munit.FunSuite
 
 import scala.collection.mutable.ListBuffer
 
-class EffectsTest extends FunSuite {
+class WeatherEffectsTest extends FunSuite {
   var player1: Player = null
   var player2: Player = null
   var board: Board = null
@@ -25,8 +25,10 @@ class EffectsTest extends FunSuite {
   var ccard3: CloseCombatCard = null
   var rcard: RangeCombatCard = null
   var rcard2: RangeCombatCard = null
+  var rcard3: RangeCombatCard = null
   var scard: SiegeCombatCard = null
   var scard2: SiegeCombatCard = null
+  var scard3: SiegeCombatCard = null
   var wfrost: WeatherCard = null
   var wrain: WeatherCard = null
   var wfog: WeatherCard = null
@@ -36,24 +38,32 @@ class EffectsTest extends FunSuite {
   var clear: ClearWeather = null
   var fog: ImpenetrableFog = null
   var rain: TorrentialRain = null
-  var bond: TightBond = null
-  var morale: MoraleBoost = null
+
+  var frost2: BitingFrost = null
+  var clear2: ClearWeather = null
+  var fog2: ImpenetrableFog = null
+  var rain2: TorrentialRain = null
 
   override def beforeEach(context: BeforeEach): Unit = {
     frost = new BitingFrost()
     clear = new ClearWeather()
     fog = new ImpenetrableFog()
     rain = new TorrentialRain()
-    bond = new TightBond()
-    morale = new MoraleBoost()
+
+    frost2 = new BitingFrost()
+    clear2 = new ClearWeather()
+    fog2 = new ImpenetrableFog()
+    rain2 = new TorrentialRain()
     
-    ccard = new CloseCombatCard("Funny", 9, Some(morale))
+    ccard = new CloseCombatCard("Funny", 9)
     ccard2 = new CloseCombatCard("Pepe", 3)
     ccard3 = new CloseCombatCard("Pepa", 15)
-    rcard = new RangeCombatCard("Fauna", 7, Some(bond))
-    rcard2 = new RangeCombatCard("Fauna", 7, Some(bond))
-    scard = new SiegeCombatCard("Funny", 6)
-    scard2 = new SiegeCombatCard("Funny", 6)
+    rcard = new RangeCombatCard("Fauna", 7)
+    rcard2 = new RangeCombatCard("Fauna", 7)
+    rcard3 = new RangeCombatCard("Mumei", 2)
+    scard = new SiegeCombatCard("Elmo", 6)
+    scard2 = new SiegeCombatCard("Perkin", 4)
+    scard3 = new SiegeCombatCard("Perkin", 8)
     wfrost = new WeatherCard("Wes", frost)
     wfog = new WeatherCard("Mel", fog)
     wrain = new WeatherCard("Jodio", rain)
@@ -67,29 +77,52 @@ class EffectsTest extends FunSuite {
     hand += rcard
     hand += rcard2
     hand += scard
+    hand += scard2
     hand += wfrost
     hand += wfog
     hand2 = new ListBuffer[Card]
-    hand2 += rcard2
-    hand2 += scard2
+    hand2 += rcard3
+    hand2 += scard3
+    hand2 += ccard3
     hand2 += wrain
     hand2 += wclear
-    hand2 += ccard2
     
     board = new Board
     player1 = new Player("p1", board, deck, hand, 1)
     player2 = new Player("p2", board, deck2, hand2, 1)
   }
   
+  test("Equals and Hash methods") {
+    assertEquals(frost,frost2)
+    assertEquals(fog,fog2)
+    assertEquals(rain,rain2)
+    assertEquals(clear,clear2)
+
+    assertEquals(frost.##, frost2.##)
+    assertEquals(fog.##, fog2.##)
+    assertEquals(rain.##, rain2.##)
+    assertEquals(clear.##, clear2.##)
+  }
+  
   test("BitingFrost") {
     player1.playCard(ccard)
     player1.playCard(ccard2)
     player1.playCard(rcard)
+    player1.playCard(rcard2)
+    player1.playCard(scard)
+    player1.playCard(scard2)
     player2.playCard(ccard3)
+    player2.playCard(scard3)
+    player2.playCard(rcard3)
     player1.playCard(wfrost)
     board.section1.closeCombatZone.foreach(x => assertEquals(x.getStrength,1))
     board.section2.closeCombatZone.foreach(x => assertEquals(x.getStrength,1))
+    
     board.section1.rangeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    board.section2.rangeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    
+    board.section1.siegeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    board.section2.siegeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
   }
 
   test("ClearWeather") {
@@ -112,36 +145,44 @@ class EffectsTest extends FunSuite {
   }
 
   test("ImpenetrableFog") {
+    player1.playCard(ccard)
+    player1.playCard(ccard2)
     player1.playCard(rcard)
-    player2.playCard(rcard2)
-    player2.playCard(scard2)
+    player1.playCard(rcard2)
+    player1.playCard(scard)
+    player1.playCard(scard2)
+    player2.playCard(ccard3)
+    player2.playCard(scard3)
+    player2.playCard(rcard3)
     player1.playCard(wfog)
+    board.section1.closeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    board.section2.closeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+
     board.section1.rangeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
     board.section2.rangeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
+
+    board.section1.siegeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
     board.section2.siegeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
   }
 
   test("TorrentialRain") {
-    player1.playCard(scard)
-    player2.playCard(scard2)
-    player2.playCard(rcard2)
-    player2.playCard(wrain)
-    board.section1.siegeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
-    board.section2.siegeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
-    board.section2.rangeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
-  }
-
-  test("MoraleBoost") {
-    player1.playCard(ccard2)
     player1.playCard(ccard)
-    assertEquals(board.section1.closeCombatZone.head.getStrength, ccard2.getOriginalStrength + 1)
-    assertEquals(board.section1.closeCombatZone(1).getStrength, ccard.getOriginalStrength)
-  }
-
-  test("TightBond") {
+    player1.playCard(ccard2)
     player1.playCard(rcard)
     player1.playCard(rcard2)
-    assertEquals(board.section1.rangeCombatZone.head.getStrength, rcard.getOriginalStrength*4)
-    assertEquals(board.section1.rangeCombatZone(1).getStrength, rcard2.getOriginalStrength*2)
+    player1.playCard(scard)
+    player1.playCard(scard2)
+    player2.playCard(ccard3)
+    player2.playCard(scard3)
+    player2.playCard(rcard3)
+    player2.playCard(wrain)
+    board.section1.closeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    board.section2.closeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+
+    board.section1.rangeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+    board.section2.rangeCombatZone.foreach(x => assertNotEquals(x.getStrength, 1))
+
+    board.section1.siegeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
+    board.section2.siegeCombatZone.foreach(x => assertEquals(x.getStrength, 1))
   }
 }
